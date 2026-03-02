6dd7195d12c9f59cc6c85e57ffd38f3578cda9ec
TangYang <1553604051@qq.com>
2026-03-03 02:08:52 +0800

    add：完成了ios端集成pixelfree美颜sdk的准备工作

---
+++ ios/PixelFree.framework/Headers/SMPixelFree.h
@@ -0,0 +1,94 @@
+//
+//  SMPixelFree.h
+//  SMBeautyEngine
+//
+//  Created by mumu on 2020/7/27.
+//  Copyright © 2020 孙慕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+#import "pixelFree_c.hpp"
+NS_ASSUME_NONNULL_BEGIN
+
+__attribute__((visibility("default"))) @interface SMPixelFree : NSObject
+
+/// 初始化
+/// @param context sdk 运行的上线文，context != nil  所有着色器程序将在这里初始化， 否在视频第一帧处理时候初始化
+- (instancetype)initWithProcessContext:(EAGLContext *)context srcFilterPath:(NSString *)filterPath authFile:(NSString *)authFile;
+/*!
+ @property glContext
+ @brief OpenGL context
+ 
+ @since v1.0.0
+ */
+@property(nonatomic, strong, readonly) EAGLContext* glContext;
+
+/**
+ 处理纹理数据
+ 
+ @param texture 纹理数据
+ @param width 宽度
+ @param height 高度
+ */
+- (int)processWithTexture:(GLuint)texture width:(GLint)width height:(GLint)height rotation:(PFRotationMode)rotation;
+
+/**
+处理cpu数据
+
+@param pixelBuffer 纹理数据
+
+@param rotationMode 人脸检测方向
+
+*/
+- (int)processWithBuffer:(CVPixelBufferRef)pixelBuffer rotationMode:(PFRotationMode)rotationMode;
+
+/**
+图片处理
+
+@param image 纹理数据
+
+@param rotationMode 人脸检测方向
+
+ @return 处理后效果图
+*/
+- (UIImage *)processWithImage:(UIImage *)image rotationMode:(PFRotationMode)rotationMode;
+
+
+- (void)pixelFreeSetBeautyFilterParam:(int)key value:(void *)value;
+
+// 加载美颜bundle
+- (void)createBeautyItemFormBundleKey:(int)key data:(void*)data size:(int)sz;
+
+- (void)pixelFreeGetFaceRect:(float *)faceRect;
+
+- (int)getPixelFreeFaceNum;
+
+// 设置图片检测模式
+- (void)setDetectMode:(int)mode;
+
+// 获取是否检测到人脸
+- (int)hasFace;
+
+// 图片调色设置
+- (int)pixelFreeSetColorGrading:(PFImageColorGrading *)imageColorGrading;
+
+// HLS 功能
+- (int)pixelFreeAddHLSFilter:(PFHLSFilterParams*)HLSFilterParams;
+- (int)pixelFreeDeleteHLSFilter:(int)handle;
+- (int)pixelFreeChangeHLSFilter:(int)handle params:(PFHLSFilterParams*)HLSFilterParams;
+
+// 自定义贴纸专属
+- (void)pixelFreeSetFiterStickerWithPath:(NSString *)path;
+
+- (int)pixelFreeSetMakeupWithJsonPath:(NSString *)jsonPath;
+- (int)clearMakeup;
+
+// 设置美妆部位程度（与配置叠乘）
+- (void)pixelFreeSetMakeupPart:(int)part degree:(float)degree;
+
+@end
+
+NS_ASSUME_NONNULL_END
